2b23765202a853992497462bf26026c5ef525040
karvel-code <velgithinji@gmail.com>
2021-07-20 16:49:54 +0300

    Add validations for cost and group models

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,5 +1,7 @@
 class Cost < ApplicationRecord
-    belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User'
   has_many :cost_groups
   has_many :groups, through: :cost_groups
+
+  validates :name, presence: true, length: { minimum: 3, maximun: 25 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,6 @@ class Group < ApplicationRecord
     has_many :cost_groups
   
     has_many :cost, through: :cost_groups
+
+    validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 end
