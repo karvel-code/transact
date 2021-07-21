88a5e21bfc6bc703013acdd64eef5469f284c661
karvel-code <velgithinji@gmail.com>
2021-07-21 08:19:14 +0300

    Add tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'creates user correctly' do
+    @user = User.new(username: 'elvis', email: 'elvis@gmail.com', password: 'elvis123')
+    expect(@user.valid?).to be true
+  end
+
+  it 'user must have email' do
+    @user = User.new(username: 'elvis', password: 'elvis123')
+    expect(@user.valid?).to be false
+  end
+
+  it 'user must have password' do
+    @user = User.new(username: 'elvis', email: 'elvis@gmail.com')
+    expect(@user.valid?).to be false
+  end
+end
