9b2d0082f2877bdd82d5636ff0b45019b5d96ade
karvel-code <velgithinji@gmail.com>
2021-07-22 13:08:39 +0300

    Add functionality to the view

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -3,8 +3,15 @@ class CostsController < ApplicationController
     end
 
     def new
+        @group_options = Group.all.map{|group_option|[group_option.name]}
+        @cost = Cost.new
     end
 
     def create
     end
+
+    private
+    def cost_params
+        params.require(:cost).permit(:name, :amount)
+    end 
 end
