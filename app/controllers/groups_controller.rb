9235fdfe40d5e19b0216440692a38dcae9ea70c1
karvel-code <velgithinji@gmail.com>
2021-07-22 09:20:50 +0300

    Add create action in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,15 @@ class GroupsController < ApplicationController
     end
 
     def create
+        @group = current_user.groups.build(group_params)
+
+        if @group.save
+            flash[:success] = "#{@group.name} Group has been created successfully!"
+            redirect_to groups_path
+          else
+            flash.now[:error] = @group.errors.full_messages
+            render :new
+        end
     end
 
     private
