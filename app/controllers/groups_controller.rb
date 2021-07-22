ea0a36a5f483eeef80160f5421ad11f0efdf8bda
karvel-code <velgithinji@gmail.com>
2021-07-22 09:14:55 +0300

    Modify groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,15 @@
 class GroupsController < ApplicationController
+    before_action :authenticate_user!
     def new
         @group = Group.new
+    end
+
+    def create
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name)
     end
 end
