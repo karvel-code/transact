314fc401b22d56929395da6c54163ea600b78d94
karvel-code <velgithinji@gmail.com>
2021-07-26 12:27:21 +0300

    Add image association in group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,5 +5,7 @@ class Group < ApplicationRecord
   
     has_many :costs, through: :cost_groups, source: :cost
 
+    has_one_attached :image
+
     validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 end
