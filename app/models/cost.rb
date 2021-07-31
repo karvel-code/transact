e5c1be2b5d28ee57f7159b87e413e43bf88ebad8
karvel-code <velgithinji@gmail.com>
2021-07-31 11:49:16 +0300

    Modify method in cost model

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -5,14 +5,8 @@ class Cost < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 
-  #   has_many :cost_groups_a, -> { where group_id: 0 }, class_name: 'CostGroup', foreign_key: 'cost_id'
-  #   has_many :grouped_costs, through: :
-
-  # scope :internal, -> { where('group_id > ?', 0 ) }
-  # scope :external, -> { where('group_id = ?', nil ) }
-
   def self.internal
-    Cost.select { |c| !c.groups.empty? }
+    Cost.reject { |c| c.groups.empty? }
   end
 
   def self.external
