32c7ab96914264227983f62c0c013ad3d50ddc06
karvel-code <velgithinji@gmail.com>
2021-08-02 13:40:52 +0300

    Add show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = current_user.username
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :users, only: %i[create index]
+  resources :users, only: %i[create index show]
   resources :costs, except: [:show]
   resources :groups
   get 'users/index'
