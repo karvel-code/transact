f235c15a64040e47db27702f48645f91cfa59bed
karvel-code <velgithinji@gmail.com>
2021-08-02 17:03:51 +0300

    Add more validation tests for groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
 
-  let(:user1) { User.create(name: 'elvis', email: 'elvis@gmail.com', password: 'elvis123') }
+  let(:user1) { User.create(username: 'elvis', email: 'elvis@gmail.com', password: 'elvis123') }
 
   context 'validation test' do
     it 'is not valid without valid params' do
@@ -10,7 +10,15 @@ RSpec.describe Group, type: :model do
       expect(group1.valid?).to be false
     end
 
+    it 'group must have a name' do
+      @group = Group.new(name: '', author_id: user1.id)
+      expect(@group.valid?).to be false
+    end
 
+    it 'group must have an author id' do
+      @group = Group.new(name: 'taxes', author_id: nil)
+      expect(@group.valid?).to be false
+    end
   
   end
 
